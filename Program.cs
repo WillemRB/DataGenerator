ec1fba3ceada6c634d5feecd97c4209cf50d7d3b
WillemRB <wrb@outlook.com>
2016-01-14 11:43:53 +0100

    Add configuration options. Trying out Fare

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,14 @@ namespace DataGenerator
     {
         static void Main(string[] args)
         {
+            var gen = new Fare.Xeger(ConfigurationSettings.AppSettings["pattern"]);
+
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine(gen.Generate());
+            }
+
+            Console.ReadLine();
         }
     }
 }
